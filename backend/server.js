0fe6f6c7a06d345fd7579b4f8ecfe5a446f0d8a6
Garvit <taneja.garvit29@gmail.com>
2025-05-11 23:44:34 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -43,5 +43,6 @@ app.use(errorHandler);
 // Start Server
 const PORT = process.env.PORT || 5000;
 mongoConnect().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
 });
